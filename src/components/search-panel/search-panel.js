a73e9c7a65c1a7ba7bd5c42d750573cf9633f080
vad <example@post.ua>
2021-04-01 11:25:47 +0300

    123

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Searchpanel = () => {
+    return (
+        <input
+            className="form-control search-input"
+            type="text"
+            placeholder="Поиск по записям"
+        />
+    )
+}
+
+export default Searchpanel;
