dc4a013b0a4fe8d6fb74372b7272a4c118ea2cf2
vad <example@post.ua>
2021-04-02 16:16:49 +0300

    update

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,18 +7,31 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 1},
+        {label: 'That is so good', important: false, id: 2},
+        {label: 'I need a break...', important: false, id: 3}
+    ];
+
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
